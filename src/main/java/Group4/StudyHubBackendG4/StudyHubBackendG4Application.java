305bf4e669fbb2412d637561354d2299c879bc6e
colo102 <javierydel98@gmail.com>
2024-03-22 16:24:39 -0300

    Initial Scaffolding

---
+++ src/main/java/Group4/StudyHubBackendG4/StudyHubBackendG4Application.java
@@ -0,0 +1,13 @@
+package Group4.StudyHubBackendG4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHubBackendG4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHubBackendG4Application.class, args);
+	}
+
+}
---
+++ src/test/java/Group4/StudyHubBackendG4/StudyHubBackendG4ApplicationTests.java
@@ -0,0 +1,13 @@
+package Group4.StudyHubBackendG4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHubBackendG4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
