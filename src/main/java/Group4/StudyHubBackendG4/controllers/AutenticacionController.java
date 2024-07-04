68eab769736d5566859257d888cc7aa9f2099adb
Pablo Cristiani <127637034+pcristiani@users.noreply.github.com>
2024-07-04 20:35:55 -0300

    cambios en swagger, descripcion controladores y endpoints
    cambios en swagger, descripcion controladores y endpoints

--- src/main/java/Group4/StudyHubBackendG4/controllers/AutenticacionController.java
+++ src/main/java/Group4/StudyHubBackendG4/controllers/AutenticacionController.java
@@ -3,6 +3,8 @@ package Group4.StudyHubBackendG4.controllers;
 import Group4.StudyHubBackendG4.datatypes.DtLoginRequest;
 import Group4.StudyHubBackendG4.persistence.Actividad;
 import Group4.StudyHubBackendG4.services.AutenticacionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +13,14 @@ import java.time.LocalDateTime;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
+@Tag(name = "Autenticacion", description = "Endpoints para la operativa de Autenticacion")
 public class AutenticacionController {
 
     @Autowired
     private AutenticacionService autenticacionService;
 
     @PostMapping("/iniciarSesion")
+    @Operation(summary = "Inicia sesion de un usuario")
     public ResponseEntity<?> login(@RequestBody DtLoginRequest loginRequest) {
         try {
             String token = autenticacionService.authenticateUser(loginRequest.getCedula(), loginRequest.getPassword());
@@ -33,6 +37,7 @@ public class AutenticacionController {
     }
 
     @PostMapping("/cerrarSesion")
+    @Operation(summary = "Cierra sesion de un usuario")
     public ResponseEntity<?> logout(@RequestBody String jwt) {
         try {
             autenticacionService.logoutUser(jwt);
